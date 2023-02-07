d1587d2ba0e98a8cad7e519a966e152b63053361
juanmarcos <acevedojuan17@gmail.com>
2023-02-06 19:35:07 -0500

    Agregando comentarios al código

--- src/app.js
+++ src/app.js
@@ -2,21 +2,25 @@ import express from 'express';
 import * as fs from 'fs';
 import { ProductManager } from './ProductManager.js';
 
+// Incluyendo Express JS.
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 
+// La variable dataBase almacenará la lista de productos que vendrá del products.json.
 let dataBase;
 
+// Trayendo base de datos del archivo products.json.
 fs.readFile('../files/products.json', 'utf-8', (error, data) => {
 	if (error) {
-		console.log(`ERROR: ${error}`)
-		return
+		console.log(`ERROR: ${error}`);
+		return;
 	}
-	const jsonData = JSON.parse(data)
-	dataBase = jsonData
+	const jsonData = JSON.parse(data);
+	dataBase = jsonData;
 })
 
+// Endpoint para mostrar todos los prodcutos y filtrados por límite.
 app.get("/products", (request, response) => {
   let limit = parseInt(request.query.limit);
   if (limit) {
@@ -32,6 +36,7 @@ app.get("/products", (request, response) => {
   };
 });
 
+// Endpoint para mostrar producto por su id.
 app.get("/products/:pid", (request, response) => {
   const product = dataBase.find(element => element.id == request.params.pid);
   if (product) {
@@ -41,6 +46,7 @@ app.get("/products/:pid", (request, response) => {
   }
 })
 
+// Información del servidor.
 const SERVER_PORT = 8080;
 
 app.listen(SERVER_PORT, () => {
--- src/testing.js
+++ src/testing.js
@@ -2,6 +2,7 @@ import { ProductManager } from './ProductManager.js';
 
 const testingProduct = new ProductManager();
 
+// Correr este script para generar un listado de productos de forma automática, indicando el número de productos que se deseen, con precios y stocks aleatorios.
 const createRandomProductList = (n) => {
   for (let i = 1; i <= n; i++) {
     testingProduct.addProduct(`Product #${i}`, `Description of product #${i}`, Math.floor(Math.random() * 20) + 0.99, `No image found for product with id ${i}`, `abc${i}`, Math.floor(Math.random() * 50) + 1);
