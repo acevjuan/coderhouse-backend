2ac735339e90dd0136cca192cf81a3b588eac077
juanmarcos <acevedojuan17@gmail.com>
2023-02-12 15:58:32 -0500

    Creando método POST para agregar productos a carritos en router carrito.

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -16,6 +16,18 @@ fs.readFile('./files/carts.json', 'utf-8', (error, data) => {
 	cartsDb = jsonData;
 });
 
+let productsDb;
+
+// Trayendo base de datos del archivo products.json.
+fs.readFile('./files/products.json', 'utf-8', (error, data) => {
+	if (error) {
+    console.log(`ERROR: ${error}`);
+    return;
+	}
+	const jsonData = JSON.parse(data);
+	productsDb = jsonData;
+});
+
 router.post('/', (request, response) => {
   let cart = {};
   let cartId;
@@ -29,7 +41,35 @@ router.post('/', (request, response) => {
   fs.writeFile('./files/carts.json', JSON.stringify(cartsDb), (err) => {
     console.log(err);
   });
-  response.send({ status: 'Success!', message: 'Cart created succesfully'} );
+  return response.send({ status: 'Success!', message: 'Cart created succesfully'} );
 });
+
+router.post('/:cid/product/:pid', (request, response) => {
+  let cartId = parseInt(request.params.cid);
+  let productId = parseInt(request.params.pid);
+  const cartExists = cartsDb.find(c => c.id === cartId);
+  const productExists = productsDb.find(p => p.id === productId);;
+  if (!cartExists || !productExists) {
+    return response.send({ message: "no existe" });
+  };
+  let cartIndex = cartsDb.findIndex(crt => crt.id == cartId);
+  const productInCart = cartsDb[cartIndex].products.find(p => p.id == productId);
+  if (productInCart) {
+    const productInCartIndex = cartsDb[cartIndex].products.findIndex(p => p.id == productId);
+    cartsDb[cartIndex].products[productInCartIndex].quantity = cartsDb[cartIndex].products[productInCartIndex].quantity + 1;
+    fs.writeFile('./files/carts.json', JSON.stringify(cartsDb), (err) => {
+      console.log(err);
+    });
+    console.log('Nuevo listado de carrito: ', cartsDb);
+    return response.send(cartsDb);
+  } else {
+    let product = { id: productId, quantity: 1 };
+    cartsDb[cartIndex].products.push(product);
+    fs.writeFile('./files/carts.json', JSON.stringify(cartsDb), (err) => {
+      console.log(err);
+    });
+    return response.send(cartsDb);
+  }
+})
 
 export default router;
