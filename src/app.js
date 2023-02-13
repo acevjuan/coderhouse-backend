d0abd7256eab3ce84857166169e1f360dfa00b9b
juanmarcos <acevedojuan17@gmail.com>
2023-02-12 19:48:53 -0500

    Refactorizando y comentando código en archivos app.js y products.router.js.

--- src/app.js
+++ src/app.js
@@ -1,23 +1,25 @@
-// Importando librerías Express JS.
+// Importando Express JS al proyecto.
 import express from 'express';
 
 //Importando routers de products y carts.
 import productsRouter from './routes/products.router.js';
 import cartsRouter from './routes/carts.router.js';
 
-// Incluyendo Express JS.
+// Incluyendo Express JS en el archivo app.js.
 const app = express();
 
+// Incluyendo middlewares express.json() y express.urlencoded().
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Llamando routers
+// Llamando routers products y carts.
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
 
 // Información del servidor.
 const SERVER_PORT = 8080;
 
+// Inicializando servidor.
 app.listen(SERVER_PORT, () => {
   console.log(`Listening server on port ${SERVER_PORT}`);
 });
--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,11 +1,14 @@
+// Importando Router para utilizar los routers creados para el proyecto, así como File System para manejar archivos de base de datos json.
 import { Router } from "express";
 import * as fs from 'fs';
 
+// Para esta entrega no se utilizó esta clase.
 import { ProductManager } from '../classes/ProductManager.js';
 
+// Trayendo router.
 const router = Router();
 
-// La variable productsDb almacenará la lista de productos que vendrá del products.json.
+// La variable productsDb traerá y actualizará información en base de datos products.json.
 let productsDb;
 
 // Trayendo base de datos del archivo products.json.
@@ -18,7 +21,7 @@ fs.readFile('./files/products.json', 'utf-8', (error, data) => {
 	productsDb = jsonData;
 });
 
-// Endpoint para mostrar todos los prodcutos y filtrados por límite.
+// Endpoint para mostrar todos los productos y filtrados por límite.
 router.get("/", (request, response) => {
   let limit = parseInt(request.query.limit);
   if (limit) {
@@ -28,9 +31,9 @@ router.get("/", (request, response) => {
         listLimit.push(productsDb[i - 1]);
       };
     };
-    response.send(listLimit);
+    response.send({ status: 'Success', message: `Showing a list of ${limit} products`, payload: listLimit});
   } else {
-    response.send(productsDb);
+    response.send({ status: 'Success', message: 'Showing all products', payload: productsDb} );
   };
 });
 
@@ -38,12 +41,13 @@ router.get("/", (request, response) => {
 router.get("/:pid", (request, response) => {
   const product = productsDb.find(element => element.id == request.params.pid);
   if (product) {
-    response.send(product);
+    response.send( {status: 'Success', message: `Showing product with id ${request.params.pid}`, payload: product});
   } else {
-    response.send({ message: `Couldn't find a product with id ${request.params.pid}` });
+    response.send({ status: 'Error', message: `Couldn't find a product with id ${request.params.pid}` });
   };
 });
 
+// Endpoint para crear un nuevo producto y registrarlo en la base de datos products.json.
 router.post('/', (request, response) => {
   let product = {};
   let productId;
@@ -54,51 +58,55 @@ router.post('/', (request, response) => {
   };
   product = {id: productId, code: `abc${productId}`, ...request.body, thumbnails: []};
   if (!product.id || !product.title || !product.description || !product.code || !product.price || product.status === undefined || !product.stock || !product.category || !product.thumbnails) {
-    console.error('Not enough information.');
     response.status(400).send({ status: 'Error', message: 'Not enough information.' });
   };
   productsDb.push(product);
-  response.send({ status: 'Success', message: 'Product created succesfully', payload: product} );
   fs.writeFile('./files/products.json', JSON.stringify(productsDb), (err) => {
     console.log(err);
   });
+  response.send({ status: 'Success', message: 'Product created successfully', payload: product} );
 });
 
+// Endpoint para modficar la información de algún producto existente indicando su id.
 router.put('/:pid', (request, response) => {
   let productId = parseInt(request.params.pid);
   let productToUpdate = request.body;
   if(productToUpdate.id) {
     return response.send({ status: 'Error', message: 'id cannot be updated.' });
   };
-  const productPosition = productsDb.findIndex((p => p.id === productId));
-  console.log(productId);
-  if (productPosition < 0) {
-    return response.status(202).send({ status: 'info', error: 'Usuario no encontrado' });
+  if(productToUpdate.code) {
+    return response.send({ status: 'Error', message: 'Code cannot be updated.' });
   };
-  if (productToUpdate.title && productToUpdate.description && productToUpdate.price && productToUpdate.thumbnail && productToUpdate.code && productToUpdate.stock) {
+  const productPosition = productsDb.findIndex((p => p.id === productId));
+  if (productPosition < 0) {
+    return response.send({ status: 'Error', message: `Couldn't find a product with id ${productId}` });
+  };
+  if (productToUpdate.title && productToUpdate.description && productToUpdate.price && productToUpdate.thumbnails && productToUpdate.stock && productToUpdate.category && (productToUpdate.status === true || productToUpdate.status === false)) {
     productToUpdate.id = productsDb[productPosition].id;
+    productToUpdate.code = productsDb[productPosition].code;
     productsDb[productPosition] = productToUpdate;
     fs.writeFile('./files/products.json', JSON.stringify(productsDb), (err) => {
       console.log(err);
     });
-    return response.send({ status: 'Success!', message: 'Product updated'});
+    return response.send({ status: 'Success', message: 'Product updated successfully', payload: productToUpdate});
   } else {
-    return response.status(202).send({ status: 'info', error: 'Not enough information.' });
+    return response.send({ status: 'Error', message: 'Not enough information.' });
   }
 });
 
+// Endepoint para eliminar producto según id indicado.
 router.delete('/:pid', (request, response) => {
   let productId = parseInt(request.params.pid);
   let productExists = productsDb.find(p => p.id === productId);
   if (!productExists) {
-    return response.status(202).send({ status: 'info', error: 'error' });
+    return response.status(202).send({ status: 'Error', message: `Couldn't find a product with id ${productId}` });
   };
   let productIndex = productsDb.findIndex(p => p.id === productId);
   productsDb.splice(productIndex, 1);
   fs.writeFile('./files/products.json', JSON.stringify(productsDb), (err) => {
     console.log(err);
   });
-  return response.send({ status: 'Success!', message: 'Product deleted'});
+  return response.send({ status: 'Success', message: 'Product deleted successfully' });
 })
 
 export default router;
