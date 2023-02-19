aaf9133ca48da14ccd15e75673750454887359ff
juanmarcos <acevedojuan17@gmail.com>
2023-02-19 12:41:10 -0500

    Configurando carpetas y archivos para Handlebars y Socket.io.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
-// Importando Express JS al proyecto.
+// Importando librerías al pryecto (Express JS, Handlebars y Socket.io).
 import express from 'express';
+import __dirname from './utils.js';
+import handlebars from 'express-handlebars';
+import viewsRouter from './routes/views.router.js';
+import { Server } from 'socket.io';
 
 //Importando routers de products y carts.
 import productsRouter from './routes/products.router.js';
@@ -12,14 +16,29 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Handlebars
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname+'/views');
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname+'/public'));
+app.use('/', viewsRouter);
+
 // Llamando routers products y carts.
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
+app.use('/api/realtimeproducts', viewsRouter);
 
 // Información del servidor.
 const SERVER_PORT = 8080;
 
 // Inicializando servidor.
-app.listen(SERVER_PORT, () => {
+const httpServer = app.listen(SERVER_PORT, () => {
   console.log(`Listening server on port ${SERVER_PORT}`);
+});
+
+// Socket server
+const socketServer = new Server(httpServer);
+
+socketServer.on('connection', socket => {
+  console.log('Nuevo cliente conectado');
 });
